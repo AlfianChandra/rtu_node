03d6ccb740bb7c8541258bf3194146c9c1609ef5
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-02-10 11:16:56 +0700

    -
    -

--- controllers/api/v1Controller.js
+++ controllers/api/v1Controller.js
@@ -4,14 +4,16 @@ const client = new MongoClient(atlasUri, { useNewUrlParser: true, useUnifiedTopo
 
 const savePreset = async (req, res) => { 
       const savedata = req.body
+      //Generate unique id for identifier
+      savedata['key'] = Math.random().toString(36).substring(7);
       try {
             await client.connect();
             const db = client.db("folsaves");
             const collection = db.collection("savedata");
             await collection.insertOne(savedata);
-            res.status(200).send({ message: 'Preset saved' });
+            res.status(200).send({ status:200, message: 'Preset saved' });
       } catch (error) {
-            res.status(500).send({ message: 'Error saving preset' });
+            res.status(500).send({ status:500, message: 'Error saving preset' });
       } finally { 
             await client.close();
       }
