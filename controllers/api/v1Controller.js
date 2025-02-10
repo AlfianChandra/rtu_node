73498b997de79f8a1fa7cc783f6c4ba179a855c2
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-02-10 10:59:07 +0700

    Fix on savePreset
    -

--- controllers/api/v1Controller.js
+++ controllers/api/v1Controller.js
@@ -1,6 +1,20 @@
-const savePreset = (req, res) => { 
-      console.log(req.body)
-      res.json(req.body)
+const { MongoClient } = require('mongodb');
+const atlasUri = "mongodb+srv://freerhyme:Qwe123qwe@cluster0.nebai.mongodb.net/folsaves?retryWrites=true&w=majority"
+const client = new MongoClient(atlasUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const savePreset = async (req, res) => { 
+      const savedata = req.body
+      try {
+            await client.connect();
+            const db = client.db("folsaves");
+            const collection = db.collection("savedata");
+            await collection.insertOne(savedata);
+            res.status(200).send({ message: 'Preset saved' });
+      } catch (error) {
+            res.status(500).send({ message: 'Error saving preset' });
+      } finally { 
+            await client.close();
+      }
 }
 
 module.exports = { savePreset };
